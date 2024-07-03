5358245f6b71aaa73a9a24b3bb0c49dfc85ec498
dyuk3 <vaibhavsinghyo@gmail.com>
2024-07-03 17:58:37 +0530

    implement infinite scrolling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 const ProductCard = ({ data }) => {
+  // accessing the props from the parent component
   const { title, price, brand, thumbnail, rating } = data;
   return (
     <div className='w-80 border shadow-lg cursor-pointer'>
@@ -6,9 +7,9 @@ const ProductCard = ({ data }) => {
       <div className='px-12 py-4 flex flex-col gap-4'>
         <div className='text-center'>
           <p className=' text-blue-600 font-semibold'>{title}</p>
-          <p className='text-gray-500 '>{brand}</p>
+          <p className='text-gray-800 '>{brand}</p>
         </div>
-        <p className='text-gray-400 '>${price}</p>
+        <p className='text-gray-400 font-semibold'>${price}</p>
       </div>
     </div>
   );
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,18 +3,49 @@ import ProductCard from './ProductCard';
 
 const ProductList = () => {
   const [productData, setProductData] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+  const [skip, setSkip] = useState(0);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
+  // function to fetch products
   const fetchProducts = async () => {
-    const url = 'https://dummyjson.com/products';
-    const data = await fetch(url);
-    const products = await data.json();
-    console.log(products);
-    setProductData(products.products);
+    setIsLoading(true);
+    try {
+      const url = `https://dummyjson.com/products?skip=${skip}`;
+      const data = await fetch(url);
+      const products = await data.json();
+      setProductData((prevProducts) => {
+        return [...prevProducts, ...products.products];
+      });
+      setSkip((value) => value + 30);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(skip);
   };
+
+  // handler to fetch more products on scrolling down
+  const handleScroll = () => {
+    if (isLoading) return;
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      fetchProducts();
+    }
+  };
+
+  // to add eventlistener on scrolling
+  useEffect(() => {
+    if (isLoading) return;
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isLoading]);
 
   return (
     <div className='grid grid-flow-row grid-cols-4 gap-10'>
